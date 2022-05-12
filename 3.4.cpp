dec3b82c7d7b6b69fdc6b5acde48e509c9ea0f7f
hanhnguyen26 <NHHanh.2610@gmail.com>
2022-05-12 11:27:26 +0300

    Update 3.4.cpp

--- 3.4.cpp
+++ 3.4.cpp
@@ -6,23 +6,30 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void prt_mat(double ** a, int n) {                        
+void prt_mat(double ** a, int n) {            //Вывод матрицы
     int i, j;
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++)
-            cout << " " << std::setprecision(10) << std::fixed << a[i][j] << " \t";
+            cout << " " << std::setprecision(6) << std::fixed << a[i][j] << " \t";
         cout << "\n";
     }
 }
 
-void del_mat(double **a, int n){
+void prt_arr(double *a, int n){
+    for (int i = 0; i < n; i++){
+        cout << a[i] <<"    ";
+    }
+    cout << endl;
+}
+
+void del_mat(double **a, int n){            //Удаление матрицы
     for (int i = 0; i < n; i ++){
         delete[] a[i];
     }
     delete[] a;
 }
 
-void mul_mat(double **a, double *x, double *res, int n){
+void mul_mat(double **a, double *x, double *res, int n){ //Умножение матриц
     for (int i = 0; i < n; i++){
         res[i] = 0.0;
         for (int j = 0; j < n; j++){
@@ -31,16 +38,29 @@ void mul_mat(double **a, double *x, double *res, int n){
     }
 }
 
-void solve(double **a, double *b, double *prev, int n, double eps) {     
-    double *curs = new double[n];
-    double *ax = new double[n];
+double norm_mat(double **a, int n){                     //Нахождение l-нормы матрицы 
+    double sum, max = -1.0;
+    for (int i = 0; i < n; i ++){
+        sum = 0.0;
+        for (int j = 0; j < n; j++){
+            sum += std::abs(a[j][i]);
+        }
+        if (sum > max){
+            max = sum;
+        }
+    }
+    return max;
+}
 
-    for (int i = 0; i < n; i++){
+int iter(double **a, double *b, double *prev, int n, double eps) {     //Метод итераций
+    double normb = 0.0;
+    for (int i = 0; i < n; i++){                                    // матрица бета
         b[i] = b[i]/a[i][i];
         prev[i] = b[i];
+        normb += std::abs(b[i]);
     }
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){                                    // матрица альфа
         for (int j = 0; j < n; j++){
             if (i != j){
                 a[i][j] = -a[i][j]/a[i][i];
@@ -48,32 +68,47 @@ void solve(double **a, double *b, double *prev, int n, double eps) {
         }
         a[i][i] = 0.0;
     }
-    
+
+    double norma = norm_mat(a, n);
+    cout << "Matrix alpha\n";
     prt_mat(a,n);
+    cout << "Matrix alpha norm: " << norma << endl;
+    if (norma >= 1){                                                //проверка сходимости
+        return 1;
+    }
+    cout << "Matrix beta\n";
+    prt_arr(b,n);
+    cout << "Matrix beta norm: " << normb << endl;
     
+    double *curs = new double[n];
+    double *ax = new double[n];
+
     double err = 1.0;
+    int count = 0;
     while (err > eps){
+        count ++;
         err = 0.0;
         for (int i = 0; i < n; i++){
-            mul_mat(a,prev,ax,n);
-            curs[i] = b[i] + ax[i];
-            err += std::abs(curs[i] - prev[i]);
-            prev[i] = curs[i];
+            mul_mat(a,prev,ax,n);                   //ax = a*x_(k-1)
+            curs[i] = b[i] + ax[i];                 //xk = b + a*x_(k-1)
+            err += std::abs(curs[i] - prev[i]);     //Погрешность
+            prev[i] = curs[i];                      //Подготовка к следующему шагу
         }
     }
 
     cout << "\nSolution" << endl;
-    for (int i = 0; i < n; i++){
-        cout << "x" << i + 1 << " = " << curs[i] << endl;
-    }
+    prt_arr(curs,n);
 
+    cout << "\nNumber of iterations: " << count;
+    cout << "\nTheoretical number of iterations: " << log(eps/(normb/(1-norma)))/log(norma) - 1 << endl;
+    
     delete[] curs;
     delete[] ax;
+    return 0;
 }
 
 int main() {
     int i, j, n;
-    cout << "N: ";
     cin >> n;
 
     double ** a = new double * [n];
@@ -94,22 +129,22 @@ int main() {
             save_a[i][j] = a[i][j];
         }
         cin >> b[i];
-        save_b[i] = b[i];
     }         
 
     double eps;
     cin >> eps;    
 
-    solve(a,b,x,n,eps);  
-    mul_mat(save_a,x,b,n);
-    for (int i = 0; i < n; i++){
-        cout << "b" << i + 1 << " = " << b[i] << endl;
+    if (iter(a,b,x,n,eps)){
+        cout << "The iterative procedure does not converge\n";
+    } else {
+        mul_mat(save_a,x,b,n);              //Проверка 
+        cout << "\Checking" << endl;
+        prt_arr(b,n);
     }
 
     del_mat(a, n);
     del_mat(save_a, n);
     delete[] b;
-    delete[] save_b;
     delete[] x;
     return 0;
 }
